80a81a5acbf28927e25245644557493f45d105d3
Johannes Kekae <johanneskekae@gmail.com>
2023-10-23 10:28:33 +0200

    annotation

--- src/main/java/za/co/idol/assessment/repository/UserRepository.java
+++ src/main/java/za/co/idol/assessment/repository/UserRepository.java
@@ -2,8 +2,9 @@ package za.co.idol.assessment.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import za.co.idol.assessment.entities.User;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
